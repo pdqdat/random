82393187f64806ca2b7fb678bc73f367981bcd80
Dat Phan <80768235+QuocDat5@users.noreply.github.com>
2023-07-31 17:51:59 +0700

    feat: animation, randomly select a tag

--- script.js
+++ script.js
@@ -6,9 +6,20 @@ const textarea = document.getElementById("textarea");
 // automatically focus on the textarea when the page loads
 textarea.focus();
 
-// create tags when the user types in the textarea
 textarea.addEventListener("keyup", (e) => {
+    // create tags when the user types in the textarea
     createTags(e.target.value);
+
+    // check if the user has pressed the Enter key
+    if (e.key === "Enter") {
+        // wait 10ms then clear the textarea
+        setTimeout(() => {
+            e.target.value = "";
+        }, 10);
+
+        // randomly select a tag
+        randomSelect();
+    }
 });
 
 // a function to create tags
@@ -29,4 +40,54 @@ function createTags(input) {
         tagElement.innerText = tag;
         tagsElement.appendChild(tagElement);
     });
+}
+
+// a function to randomly select a tag
+function randomSelect() {
+    // the number of times to highlight a tag
+    const times = 30;
+
+    // highlight &  unhighlight a random tag every 100ms
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag();
+
+        highlightTag(randomTag);
+
+        setTimeout(() => {
+            unHighlightTag(randomTag);
+        }, 100);
+    }, 100);
+
+    // stop highlighting & unhighlighting tags after a certain amount of time
+    setTimeout(() => {
+        clearInterval(interval);
+
+        // highlight a random tag one last time after the interval has ended
+        setTimeout(() => {
+            const randomTag = pickRandomTag();
+
+            highlightTag(randomTag);
+        }, 100);
+    }, times * 100);
+}
+
+// a function to pick a random tag
+function pickRandomTag() {
+    // get all the tags
+    const tags = document.querySelectorAll(".tag");
+
+    // return a random tag by selecting a random index from the tags array using Math.random()
+    return tags[Math.floor(Math.random() * tags.length)];
+}
+
+// a function to highlight a tag
+function highlightTag(tag) {
+    // add the highlight class to the tag, the CSS will take care of the rest
+    tag.classList.add("highlight");
+}
+
+// a function to unhighlight a tag
+function unHighlightTag(tag) {
+    // remove the highlight class from the tag, the tag will go back to its original color
+    tag.classList.remove("highlight");
 }
